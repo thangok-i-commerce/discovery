bd12b6c5260cd39a531ef7dc37101a294344bbc7
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-10 00:47:40 +0700

    init eureka

---
+++ src/main/java/thangok/icommerce/discovery/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package thangok.icommerce.discovery;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thangok/icommerce/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package thangok.icommerce.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
